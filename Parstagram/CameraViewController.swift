625e9cb574495c5681decc466c801e8eb7fe0787
Stephanie <Le>
2019-11-16 21:42:48 -0600

    almost

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class CameraViewController: UIViewController, UIImagePickerControllerDelegate {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var commentField: UITextField!
@@ -22,8 +23,29 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate {
 
     @IBAction func onCameraButton(_ sender: Any) {
         let picker = UIImagePickerController()
-        picker.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        picker.delegate = self
         picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        }
+        else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+        
     }
     
     @IBAction func onSubmitButton(_ sender: Any) {
